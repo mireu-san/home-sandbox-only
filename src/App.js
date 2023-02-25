d3eaa68f45ea1067318c10887bdf9d97e25ba690
Chesed Kim <starmireu@gmail.com>
2023-02-25 17:58:21 +0900

    second commmit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import "./App.css";
-import useForm from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formStat: { isSubmitting },
+  } = useForm();
+
   return (
-    <form>
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        await new Promise((r) => setTimeout(r, 1000));
+        alert(JSON.stringify(data));
+      })}
+    >
       <label htmlFor="id">ID</label>
-      <input id="id" type="id" name="id" placeholder="input your ID" />
+      <input
+        id="id"
+        type="id"
+        name="id"
+        placeholder="input your ID"
+        {...register("email")}
+      />
       <label htmlFor="password">Password</label>
-      <input id="password" type="password" name="password" placeholder="" />
+      <input
+        id="password"
+        type="password"
+        name="password"
+        placeholder=""
+        {...register("password")}
+      />
       <button type="submit">Log in</button>
     </form>
   );
@@ -17,4 +40,4 @@ function App() {
   return <div className="App">test</div>;
 }
 
-export default App;
+export default LoginForm;
